caf2111782cb7e8d47e98e23ea18294c20b4d87b
Jędrzej Karolonek <jkarolonek@n-educatio.pl>
2018-07-26 14:29:22 +0200

    added class to check whether element is in bounds, added double click functionality

--- src/app/modules/board.js
+++ src/app/modules/board.js
@@ -11,6 +11,49 @@ class Board {
     this.displayValues();
   }
 
+
+  onClick = (event) => {
+      const field = this.getField(event);
+      this.fieldEmptyCheck(field.x, field.y);
+      this.fieldStateChange(field, 'clicked');
+      this.gameLost(field);
+
+      let checked = 0;
+
+      for (let x = 0; x < this.size; x++) {
+          for (let y = 0; y < this.size; y++) {
+              const f = this.collection[x][y];
+              if (f.state === 'clicked') {
+                  checked++;
+              }
+          }
+      }
+      console.log(this.collection);
+      if (checked === (this.size * this.size) - this.bombs) {
+          this.state = 'won';
+          this.view.classList.add('game-won');
+      }
+      console.log(this.state);
+  }
+
+  onContextMenu = (event) => {
+    const field = this.getField(event);
+    event.preventDefault();
+
+    if (field.state === 'unclicked') {
+        this.fieldStateChange(field, 'flagged');
+    } else if (field.state === 'flagged') {
+        this.fieldStateChange(field, 'unclicked');
+    }
+    console.log(this.state);
+    return false;
+  }
+
+  onDoubleClick = (event) => {
+    const field = this.getField(event);
+    this.revealAround(field.x, field.y);
+  }
+
   createBoard() {
     const collection = [];
     for (let i = 0; i < this.size; i++) {
@@ -25,10 +68,19 @@ class Board {
           field.view.addEventListener('click', this.onClick)
 
           field.view.addEventListener('contextmenu', this.onContextMenu);
+
+          field.view.addEventListener('dblclick', this.onDoubleClick);
         }
       collection.push(collectionRow);
     }
     this.collection = collection;
+  }
+
+  gameLost(field) {
+    if (field.state === 'clicked' && field.value === 9) {
+        this.state = 'lost';
+        this.view.classList.add('game-lost');
+    }
   }
 
   getField(event) {
@@ -44,6 +96,10 @@ class Board {
     field.view.classList.add(field.state);
   }
 
+  isInBounds(x,y) {
+    return x >= 0 && y >= 0 && x < this.size && y < this.size;
+  }
+
   fieldEmptyCheck(x, y) {
     if (x >= 0 && y >= 0 && x < this.size && y < this.size) {
       const tile = this.collection[x][y];
@@ -55,28 +111,28 @@ class Board {
       }
       if (tile.value === 0 && tile.state !== 'flagged') {
 
-        if (x - 1 >= 0 && y - 1 >= 0 && x - 1 < this.size && y - 1 < this.size && this.collection[x - 1][y - 1].state !== 'flagged') {
+        if (this.isInBounds(x - 1, y - 1) && this.collection[x - 1][y - 1].state !== 'flagged') {
           this.fieldStateChange(this.collection[x - 1][y - 1], 'clicked');
         }
-        if (x - 1 >= 0 && y >= 0 && x - 1 < this.size && y < this.size && this.collection[x - 1][y].state !== 'flagged') {
+        if (this.isInBounds(x - 1, y) && this.collection[x - 1][y].state !== 'flagged') {
           this.fieldStateChange(this.collection[x - 1][y], 'clicked');
         }
-        if (x - 1 >= 0 && y + 1 >= 0 && x - 1 < this.size && y + 1 < this.size && this.collection[x - 1][y + 1].state !== 'flagged') {
+        if (this.isInBounds(x - 1, y + 1) && this.collection[x - 1][y + 1].state !== 'flagged') {
           this.fieldStateChange(this.collection[x - 1][y + 1], 'clicked');
         }
-        if (x >= 0 && y - 1 >= 0 && x < this.size && y - 1 < this.size && this.collection[x][y - 1].state !== 'flagged') {
+        if (this.isInBounds(x, y - 1) && this.collection[x][y - 1].state !== 'flagged') {
           this.fieldStateChange(this.collection[x][y - 1], 'clicked');
         }
-        if (x >= 0 && y + 1 >= 0 && x < this.size && y + 1 < this.size && this.collection[x][y + 1].state !== 'flagged') {
+        if (this.isInBounds(x, y + 1) && this.collection[x][y + 1].state !== 'flagged') {
           this.fieldStateChange(this.collection[x][y + 1], 'clicked');
         }
-        if (x + 1 >= 0 && y - 1 >= 0 && x + 1 < this.size && y - 1 < this.size && this.collection[x + 1][y - 1].state !== 'flagged') {
+        if (this.isInBounds(x + 1, y - 1) && this.collection[x + 1][y - 1].state !== 'flagged') {
           this.fieldStateChange(this.collection[x + 1][y - 1], 'clicked');
         }
-        if (x + 1 >= 0 && y >= 0 && x + 1 < this.size && y < this.size && this.collection[x + 1][y].state !== 'flagged') {
+        if (this.isInBounds(x + 1, y) && this.collection[x + 1][y].state !== 'flagged') {
           this.fieldStateChange(this.collection[x + 1][y], 'clicked');
         }
-        if (x + 1 >= 0 && y + 1 >= 0 && x + 1 < this.size && y + 1 < this.size && this.collection[x + 1][y + 1].state !== 'flagged') {
+        if (this.isInBounds(x + 1, y + 1) && this.collection[x + 1][y + 1].state !== 'flagged') {
           this.fieldStateChange(this.collection[x + 1][y + 1], 'clicked');
         }
 
@@ -91,47 +147,77 @@ class Board {
         
       }
     }
-    
   }
 
-  onClick = (event) => {
-    const field = this.getField(event);
-    this.fieldEmptyCheck(field.x, field.y);
-    this.fieldStateChange(field, 'clicked');
-    if (field.state === 'clicked' && field.value === 9) {
-      this.state = 'lost';
-      this.view.classList.add('game-lost');
+  revealAround(x, y) {
+    const tile = this.collection[x][y];
+    let flagCount = 0;
+    if (this.isInBounds(x - 1, y - 1) && this.collection[x - 1][y - 1].state === 'flagged') {
+        flagCount++;
     }
-
-    let checked = 0;
-
-    for (let x = 0; x < this.size; x++) {
-      for (let y = 0; y < this.size; y++) {
-        const f = this.collection[x][y];
-        if (f.state === 'clicked') {
-          checked++;
-        }
+    if (this.isInBounds(x - 1, y) && this.collection[x - 1][y].state === 'flagged') {
+        flagCount++;
+    }
+    if (this.isInBounds(x - 1, y + 1) && this.collection[x - 1][y + 1].state === 'flagged') {
+        flagCount++;
+    }
+    if (this.isInBounds(x, y - 1) && this.collection[x][y - 1].state === 'flagged') {
+        flagCount++;
+    }
+    if (this.isInBounds(x, y + 1) && this.collection[x][y + 1].state === 'flagged') {
+        flagCount++;
+    }
+    if (this.isInBounds(x + 1, y - 1) && this.collection[x + 1][y - 1].state === 'flagged') {
+        flagCount++;
+    }
+    if (this.isInBounds(x + 1, y) && this.collection[x + 1][y].state === 'flagged') {
+        flagCount++;
+    }
+    if (this.isInBounds(x + 1, y + 1) && this.collection[x + 1][y + 1].state === 'flagged') {
+        flagCount++;
+    }
+    if (tile.value === flagCount) {
+      if (this.isInBounds(x - 1, y - 1) && this.collection[x - 1][y - 1].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x - 1][y - 1], 'clicked');
+          this.gameLost(this.collection[x - 1][y - 1]);
+          this.fieldEmptyCheck(x - 1, y - 1);
+      }
+      if (this.isInBounds(x - 1, y) && this.collection[x - 1][y].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x - 1][y], 'clicked');
+          this.gameLost(this.collection[x - 1][y]);
+          this.fieldEmptyCheck(x - 1, y);
+      }
+      if (this.isInBounds(x - 1, y + 1) && this.collection[x - 1][y + 1].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x - 1][y + 1], 'clicked');
+          this.gameLost(this.collection[x - 1][y + 1]);
+          this.fieldEmptyCheck(x - 1, y + 1);
+      }
+      if (this.isInBounds(x, y - 1) && this.collection[x][y - 1].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x][y - 1], 'clicked');
+          this.gameLost(this.collection[x][y - 1]);
+          this.fieldEmptyCheck(x, y - 1);
+      }
+      if (this.isInBounds(x, y + 1) && this.collection[x][y + 1].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x][y + 1], 'clicked');
+          this.gameLost(this.collection[x][y + 1]);
+          this.fieldEmptyCheck(x, y + 1);
+      }
+      if (this.isInBounds(x + 1, y - 1) && this.collection[x + 1][y - 1].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x + 1][y - 1], 'clicked');
+          this.gameLost(this.collection[x + 1][y - 1]);
+          this.fieldEmptyCheck(x + 1, y - 1);
+      }
+      if (this.isInBounds(x + 1, y) && this.collection[x + 1][y].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x + 1][y], 'clicked');
+          this.gameLost(this.collection[x + 1][y]);
+          this.fieldEmptyCheck(x + 1, y);
+      }
+      if (this.isInBounds(x + 1, y + 1) && this.collection[x + 1][y + 1].state !== 'flagged') {
+          this.fieldStateChange(this.collection[x + 1][y + 1], 'clicked');
+          this.gameLost(this.collection[x + 1][y + 1]);
+          this.fieldEmptyCheck(x + 1, y + 1);
       }
     }
-    console.log(this.collection);
-    if (checked === (this.size * this.size) - this.bombs) {
-      this.state = 'won';
-      this.view.classList.add('game-won');
-    }
-    console.log(this.state);
-  }
-
-  onContextMenu = (event) => {
-    const field = this.getField(event);
-    event.preventDefault();
-    
-    if (field.state === 'unclicked') {
-      this.fieldStateChange(field, 'flagged');
-    } else if (field.state === 'flagged') {
-      this.fieldStateChange(field, 'unclicked');
-    }
-    console.log(this.state)
-    return false;
   }
 
   randomizeBombs() {
@@ -148,40 +234,42 @@ class Board {
     }
     while (bombsToDraw > 0);
   }
+
   assignHints() {
     for(let x = 0; x < this.size; x++) {
       for(let y = 0; y < this.size; y++) {
         if (this.collection[x][y].value === 9) {
           continue;
         }
-        if (x - 1 >= 0 && y - 1 >= 0 && x - 1 < this.size && y - 1 < this.size && this.collection[x - 1][y - 1].value === 9) {
+        if (this.isInBounds(x - 1, y - 1) && this.collection[x - 1][y - 1].value === 9) {
           this.collection[x][y].value++
         }
-        if (x - 1 >= 0 && y + 1 >= 0 && x - 1 < this.size && y + 1 < this.size && this.collection[x - 1][y + 1].value === 9) {
+        if (this.isInBounds(x - 1, y) && this.collection[x - 1][y].value === 9) {
           this.collection[x][y].value++
         }
-        if (x + 1 >= 0 && y + 1 >= 0 && x + 1 < this.size && y + 1 < this.size && this.collection[x + 1][y + 1].value === 9) {
+        if (this.isInBounds(x - 1, y + 1) && this.collection[x - 1][y + 1].value === 9) {
           this.collection[x][y].value++
         }
-        if (x + 1 >= 0 && y - 1 >= 0 && x + 1 < this.size && y - 1 < this.size && this.collection[x + 1][y - 1].value === 9) {
+        if (this.isInBounds(x, y - 1) && this.collection[x][y - 1].value === 9) {
           this.collection[x][y].value++
         }
-        if (x - 1 >= 0 && y >= 0 && x - 1 < this.size && y < this.size && this.collection[x - 1][y].value === 9) {
+        if (this.isInBounds(x, y + 1) && this.collection[x][y + 1].value === 9) {
           this.collection[x][y].value++
         }
-        if (x >= 0 && y - 1 >= 0 && x < this.size && y - 1 < this.size && this.collection[x][y - 1].value === 9) {
+        if (this.isInBounds(x + 1, y - 1) && this.collection[x + 1][y - 1].value === 9) {
           this.collection[x][y].value++
         }
-        if (x + 1 >= 0 && y >= 0 && x + 1 < this.size && y < this.size && this.collection[x + 1][y].value === 9) {
+        if (this.isInBounds(x + 1, y) && this.collection[x + 1][y].value === 9) {
           this.collection[x][y].value++
         }
-        if (x >= 0 && y + 1 >= 0 && x < this.size && y + 1 < this.size && this.collection[x][y + 1].value === 9) {
+        if (this.isInBounds(x + 1, y + 1) && this.collection[x + 1][y + 1].value === 9) {
           this.collection[x][y].value++
         }
       }
     }
     console.log(this.collection);
   }
+
   displayValues() {
     for(let x = 0; x < this.size; x++) {
       for(let y = 0; y < this.size; y++) {
