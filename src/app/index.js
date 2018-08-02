e435db8f4f56712a7d55a2de69493b77158f7547
Jędrzej Karolonek <jkarolonek@DESKTOP-O4VTDAF.localdomain>
2018-08-02 14:56:50 +0200

    added custom game functionality

--- src/app/index.js
+++ src/app/index.js
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
   let board = new Board(boardContainer, bombCount, boardWidth, boardHeight);
 
   const restartBtn = document.querySelector('.diff-btn');
+  const customBtn = document.querySelector('.custom-btn');
 
   const options = document.getElementsByClassName('diff');
-
 
   restartBtn.addEventListener('click', () => {
     board.removeBoard();
@@ -32,5 +32,24 @@ document.addEventListener('DOMContentLoaded', () => {
     style.setProperty('--width', boardWidth);
     style.setProperty('--height', boardHeight);
     board = new Board(boardContainer, bombCount, boardWidth, boardHeight);
+  });
+
+  customBtn.addEventListener('click', () => {
+    const customWidth = document.getElementById('custom-width').value;
+    const customHeight = document.getElementById('custom-height').value;
+    const customBombs = document.getElementById('bomb-number').value;
+    if (customWidth === '' || customHeight === '' || customBombs === '') {
+      alert('Ooops, you left some fields empty, cannot generate board!');
+    } else if (customWidth * customHeight < customBombs) {
+      alert('Ooops, the number of bombs exceeds board size, cannot generate board!');
+    } else {
+      board.removeBoard();
+      boardWidth = customWidth;
+      boardHeight = customHeight;
+      bombCount = customBombs;
+      style.setProperty('--width', boardWidth);
+      style.setProperty('--height', boardHeight);
+      board = new Board(boardContainer, bombCount, boardWidth, boardHeight);
+    }
   });
 });
